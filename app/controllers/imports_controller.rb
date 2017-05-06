b8fd54fa11cd5db5f267d9e973a7f87110b63300
BLauris <bligzna.lauris@gmail.com>
2017-05-06 17:42:36 +0300

    Created controller for importing functionality
    Will move logic from car controller that is responsible for importing to
    this new controller.

---
+++ app/controllers/imports_controller.rb
@@ -0,0 +1,25 @@
+class ImportsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def preview
+
+  end
+
+  def import
+
+  end
+
+  private
+
+    def import_params
+
+    end
+
+end
